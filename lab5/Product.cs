c6f443fe338533147834efbfc92a597c4160b28a
Никита <@SANTICUM>
2023-12-05 10:54:36 +0200

    Добавьте файлы проекта.

---
+++ lab5/Product.cs
@@ -0,0 +1,106 @@
+﻿interface ISearchable<T>
+{
+    List<T> Search(Func<T, bool> criteria);
+}
+
+// Interface for orderable items
+interface IOrderable
+{
+    double CalculateTotal();
+}
+
+// Class representing a Product
+class Product : IOrderable
+{
+    public string Name { get; private set; }
+    public double Price { get; private set; }
+    public string Description { get; private set; }
+    public string Category { get; private set; }
+    public int Rating { get; private set; }
+
+    public Product(string name, double price, string description, string category, int rating)
+    {
+        Name = name;
+        Price = price;
+        Description = description;
+        Category = category;
+        Rating = rating;
+    }
+
+    public double CalculateTotal()
+    {
+        return Price;
+    }
+}
+
+// Class representing a User
+class User
+{
+    public string Username { get; private set; }
+    private string Password { get; set; }
+    public List<Product> PurchaseHistory { get; private set; } = new List<Product>();
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
+
+// Class representing an Order
+class Order : IOrderable
+{
+    public List<IOrderable> Items { get; private set; } = new List<IOrderable>();
+    public string Status { get; private set; } = "Pending";
+
+    public double CalculateTotal()
+    {
+        return Items.Sum(item => item.CalculateTotal());
+    }
+
+    public void AddItem(IOrderable item)
+    {
+        Items.Add(item);
+    }
+
+    public void CompleteOrder()
+    {
+        Status = "Completed";
+    }
+}
+
+// Class representing a ShoppingCart
+class ShoppingCart : Order
+{
+    public void Checkout(User user)
+    {
+        Console.WriteLine($"Checking out for user: {user.Username}");
+        foreach (var item in Items)
+        {
+            if (item is Product product)
+            {
+                user.PurchaseHistory.Add(product);
+            }
+        }
+
+        CompleteOrder();
+    }
+}
+
+// Class representing a Store
+class Store : ISearchable<Product>
+{
+    private List<Product> products = new List<Product>();
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public List<Product> Search(Func<Product, bool> criteria)
+    {
+        return products.Where(criteria).ToList();
+    }
+}
+
+
---
+++ lab5/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Create a store
+        Store myStore = new Store();
+
+        // Add products to the store
+        myStore.AddProduct(new Product("Laptop", 1000, "Powerful laptop", "Electronics", 4));
+        myStore.AddProduct(new Product("Book", 20, "Interesting book", "Books", 5));
+        myStore.AddProduct(new Product("Coffee Maker", 50, "Automatic coffee maker", "Appliances", 4));
+
+        // Create a user
+        User john = new User("JohnDoe", "password");
+
+        // Create a shopping cart
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        // Simulate user adding items to the shopping cart
+        List<Product> laptops = myStore.Search(p => p.Name == "Laptop");
+        if (laptops.Any())
+        {
+            shoppingCart.AddItem(laptops.First());
+        }
+
+        List<Product> books = myStore.Search(p => p.Name == "Book");
+        if (books.Any())
+        {
+            shoppingCart.AddItem(books.First());
+        }
+
+        // Simulate user checking out
+        shoppingCart.Checkout(john);
+
+        // Display user purchase history
+        Console.WriteLine($"\nPurchase History for {john.Username}:");
+        foreach (var item in john.PurchaseHistory)
+        {
+            Console.WriteLine($"{item.Name} - Price: {item.Price}");
+        }
+
+        Console.ReadLine(); // Pause to see the output
+    }
+}
+
+
